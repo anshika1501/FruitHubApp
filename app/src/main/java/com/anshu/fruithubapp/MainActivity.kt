f331171085016593530cd6ad42e9cc6367e06773
Anshika <anshikapriya12@gmail.com>
2025-06-20 20:47:14 +0530

    Initial commit

---
+++ app/src/main/java/com/anshu/fruithubapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.anshu.fruithubapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.anshu.fruithubapp.ui.theme.FruitHubAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FruitHubAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FruitHubAppTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/anshu/fruithubapp/SplashScreen.kt
@@ -0,0 +1,9 @@
+package com.anshu.fruithubapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+    Column
+}
